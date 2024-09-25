5fecb7a94995e02f40d206203ea714e6bee06564
Paula Rabelo <paula_rabelo2010@hotmail.com>
2024-09-25 15:06:36 -0300

    Criando uma async_view

---
+++ asyncviews/views.py
@@ -0,0 +1,17 @@
+import asyncio
+import httpx
+from django.http import HttpResponse
+
+async def http_call_async():
+    for num in range(1, 8):
+        await asyncio.sleep(1)
+        print(num)
+    async with httpx.AsyncClient() as client:
+        r = await client.get('https://hhtpbin.org')
+        print(r)
+
+
+async def async_view(request):
+    loop = asyncio.get_event_loop()
+    loop.create_task(http_call_async())
+    return HttpResponse('Solicitação HTTP sem bloqueio')
